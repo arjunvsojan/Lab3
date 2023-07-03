b75c762963cd5ef5ac600046c77b6b1be1e1b955
Arjun Sojan <avadakkekarayil-sojan@myseneca.ca>
2023-07-02 22:55:22 -0400

    Modified Lab3

--- lab3.py
+++ lab3.py
@@ -4,4 +4,9 @@ def create_file(name):
         file.write(name)
 #Assigning name
 my_name = "Arjun Vadakkekarayil Sojan"
-write_name_file(my_name)
+create_file(my_name)
+def helloWorld():
+	print("Hello World")
+
+helloWorld()
+
